850d959d23c555814dc7d0663855c8650640d492
vincentbharata <vincentbharataa@gmail.com>
2025-06-11 22:43:42 +0700

    Post Test

---
+++ Day 1/books/src/main/java/com/example/books/BookRequest.java
@@ -0,0 +1,12 @@
+package com.example.books;
+
+public class BookRequest {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Day 1/books/src/main/java/com/example/books/BooksApplication.java
@@ -0,0 +1,35 @@
+package com.example.books;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/books")
+public class BooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksApplication.class, args);
+	}
+
+	@GetMapping
+	public String getBooks() {
+		return "Retrieving all books";
+	}
+
+	@PostMapping
+	public String createBook(@RequestBody BookRequest request) {
+		return String.format("Book created for %s.", request.getName());
+	}
+
+	@PutMapping("/{id}")
+	public String updateBook(@PathVariable String id, @RequestBody BookRequest request) {
+		return String.format("Book with ID %s reimagined", id, request.getName());
+	}
+
+	@DeleteMapping("/{id}")
+	public String deleteBook(@PathVariable String id) {
+		return String.format("Book with ID %s removed", id);
+	}
+}
