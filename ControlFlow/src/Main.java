e192b975fa7f145305d9433319fe8261ee34759a
vincentbharata <vincentbharataa@gmail.com>
2025-06-11 14:34:27 +0700

    odd even factorial

--- ControlFlow/src/Main.java
+++ ControlFlow/src/Main.java
@@ -30,20 +30,37 @@ public class Main {
         // System.out.println("switch: " + dayName);
 
         // contoh do while
-        int i = 0;
-        do {
-            System.out.println("do while: " + i);
-            i++;
-        } while (i < 5);
-        // contoh while
-        int j = 0;
-        while (j < 5) {
-            System.out.println("while: " + j);
-            j++;
+//        int i = 0;
+//        do {
+//            System.out.println("do while: " + i);
+//            i++;
+//        } while (i < 5);
+//        // contoh while
+//        int j = 0;
+//        while (j < 5) {
+//            System.out.println("while: " + j);
+//            j++;
+//        }
+//        // contoh for
+//        for (int k = 0; k < 5; k++) {
+//            System.out.println("for: " + k);
+//        }
+        //odd-even number
+        for (int num = 1; num <= 20; num++) {
+            if (num % 2 != 0) {
+                System.out.println("odd number: " + num);
+            } else {
+                System.err.println("Even number: " + num);
+            }
         }
-        // contoh for
-        for (int k = 0; k < 5; k++) {
-            System.out.println("for: " + k);
+        //factorial
+        int factorial = 1;
+        int number = 7;
+        for (int i = 1; i <= number; i++) {
+            factorial *= i;
         }
+        System.out.println("Factorial dari " + number + " adalah: " + factorial);
+
     }
+
 }
